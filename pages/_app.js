c6696652ec58989f53d434f66d272acd8551f714
Les Kleuver <les@spatialexplorers.nl>
2021-06-07 16:42:38 +0200

    global styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
-import '../styles/globals.css'
+import { Global, css } from "@emotion/react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Global
+        styles={css`
+          html,
+          body {
+            padding: 0;
+            margin: 0;
+            background: white;
+            min-height: 100%;
+            font-family: Helvetica, Arial, sans-serif;
+            font-size: 16px;
+          }
+        `}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
